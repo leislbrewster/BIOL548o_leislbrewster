c9b96c04f1028b2e0ba19036ef9ac6e254e19aea
leislbrewster <leislbrewster1@gmail.com>
2021-02-09 20:03:47 -0800

    Updated file name

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,7 +1,9 @@
 #This function reads the file into R
-fish_data<- read.csv("Gaeta_etal_CLC_data.csv")
+fish_data<- read.csv("Gaeta_etal_CLC_data_1.csv")
 library(dplyr)
 fish_data_cat = fish_data %>% 
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 fish_data_cat = fish_data %>% 
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
