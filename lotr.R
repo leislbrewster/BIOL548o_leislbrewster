6c7cf93f41627ba405af8dae29d130d6ec4f5b67
leislbrewster <leislbrewster1@gmail.com>
2021-02-11 16:08:06 -0800

    Made data tidy

--- lotr.R
+++ lotr.R
@@ -5,5 +5,13 @@ rking <- read_csv(file.path("data", "The_Return_Of_The_King.csv"))
 rking
 lotr_untidy <- bind_rows(fship, ttow, rking)
 str(lotr_untidy)
-write_csv(lotr_untidy, path = file.path("data", "lotr_untidy"))
-
+write_csv(lotr_untidy, path = file.path("data", "lotr_untidy.csv"))
+lotr_tidy <-
+  gather(lotr_untidy, key = 'Gender', value = 'Words', Female, Male)
+lotr_tidy
+lotr_tidy <-
+  pivot_longer(lotr_untidy, cols=c(Female, Male), names_to = 'Gender', values_to = 'Words')
+lotr_tidy <- arrange(lotr_tidy, Gender)
+#writing the tidy table to the data file
+write_csv(lotr_tidy, path = file.path("data", "lotr_tidy.csv"))
+lotr_tidy
