999d13a18a79a994acb077c6fa21b87ab5fe3eb3
leislbrewster <leislbrewster1@gmail.com>
2021-03-04 15:48:28 -0800

    Lecture 7 ex 2

--- lecture7-exercises.R
+++ lecture7-exercises.R
@@ -3,6 +3,22 @@ lb_g_convert<-function(pounds){
   grams<- pounds*453.592
   return(grams)
 }
+lb_g_convert(8000)
 
 #Exercise 2
-lb_g_convert(8000)
+
+#This code calculates the mass based on allometry in Seebacher 2001
+get_mass_from_length_theropoda <- function(length){
+  mass <- 0.73 * length ^ 3.63
+  return(mass)
+}
+#mass of a Spinosaurus that is 16 m long based on its reassembled skeleton
+get_mass_from_length_theropoda(16)
+
+
+get_mass_from_length <- function(length, a, b){
+  mass <- a * length ^ b
+  return(mass)
+}
+
+get_mass_from_length(26, 214.44, 1.46)
