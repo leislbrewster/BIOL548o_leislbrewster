f6a36a3bfd4aa5262a46da99c281c04e070def66
leislbrewster <leislbrewster1@gmail.com>
2021-03-09 15:47:35 -0800

    Lecture 8 Ex 3

--- lecture8-exercises.R
+++ lecture8-exercises.R
@@ -23,3 +23,24 @@ if (age_class =="sapling"){
   y<-10
 }
 y
+
+#Exercise 3
+
+age_class <- "seedling"
+if (age_class =="sapling"){
+  y<-10
+}else{
+  y<-5
+}
+y
+
+age_class <- "adult"
+if (age_class =="sapling"){
+  y<-10
+}else if (age_class=="seedling"){
+  y<-5
+}else{
+  y<-0
+}
+
+y
